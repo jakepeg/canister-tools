b21e00e5e3d472ac55ce8e47ff49fc371ba1802c
Artur Shirokov <artur.wiseman.co@gmail.com>
2025-04-10 12:18:23 +0100

    Struggling to fix the config stuff.

--- src/canister-tools-frontend/vite.config.ts
+++ src/canister-tools-frontend/vite.config.ts
@@ -2,7 +2,9 @@ import { fileURLToPath, URL } from 'url';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import environment from 'vite-plugin-environment';
-import dotenv from 'dotenv';
+import * as dotenv from 'dotenv'; // Use namespace import
+import * as path from "path"; // Use namespace import
+import tailwindcss from "@tailwindcss/vite";
 
 dotenv.config({ path: '../../.env' });
 
@@ -27,6 +29,7 @@ export default defineConfig({
   },
   plugins: [
     react(),
+    tailwindcss(),
     environment("all", { prefix: "CANISTER_" }),
     environment("all", { prefix: "DFX_" }),
   ],
@@ -38,6 +41,11 @@ export default defineConfig({
           new URL("../declarations", import.meta.url)
         ),
       },
-    ],
+      { // Corrected alias structure
+        find: "@",
+        replacement: path.resolve(__dirname, "./src"),
+      }
+    ]
+    
   },
 });
